bb3247607edfcf843a6b47a4678a973611a9a63a
Dener Emanuel dos Santos <118932133+DenerManuel@users.noreply.github.com>
2025-05-16 17:01:30 -0300

    Delete easify-vocab-game/src directory

--- easify-vocab-game/src/styles/fonts/general.js
+++
@@ -1,4 +0,0 @@
-export const GeneralFonts = {
-  fontFamily: 'Lato',
-  defaultBackButtonFontSize: 18,
-}
--- easify-vocab-game/src/styles/fonts/index.js
+++
@@ -1,9 +0,0 @@
-import { GeneralFonts } from './general.js';
-import { ReviewScreenFonts } from './reviewScreen.js';
-import { InitialScreenFonts } from './initialScreen.js';
-
-export const Fonts = {
-  General: GeneralFonts,
-  ReviewScreen: ReviewScreenFonts,
-  InitialScreen: InitialScreenFonts,
-};
